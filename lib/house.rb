deb953922af939b8dbc864523ac6ee6cd6f6fbfc
Jake Volpe <volpe.jacob@gmail.com>
2020-12-09 13:16:09 -0700

    Finish house&h_test tests/methods

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,20 @@
 class House
-  attr_reader :price,
-              :address
+  attr_accessor :rooms
+
+  attr_reader   :price,
+                :address
+
 
   def initialize(price, address)
     @price   = price
     @address = address
+    @rooms = []
   end
+
+  def add_room(room)
+    @rooms << room
+  end
+
+
+
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -18,4 +18,22 @@ class RoomTest < Minitest::Test
     assert_equal house.address, "123 sugar lane"
   end
 
+  def test_house_starts_no_rooms
+    house = House.new("$400000", "123 sugar lane")
+
+
+    assert_equal house.rooms, []
+  end
+
+
+  def test_house_holds_proper_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal house.rooms, [room_1, room_2]
+  end
+
 end
