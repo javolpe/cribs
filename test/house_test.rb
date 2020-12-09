e1c523c9022c9d5c27bfe7eec0fec6a2b57a4992
Jake Volpe <volpe.jacob@gmail.com>
2020-12-09 13:24:35 -0700

    Write test for rooms_from_category

--- test/house_test.rb
+++ test/house_test.rb
@@ -42,5 +42,19 @@ class RoomTest < Minitest::Test
     assert_equal house.above_market_average?, false
   end
 
+  def test_show_rooms_from_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal house.rooms_from_category(:bedroom), [room_1, room_2]
+  end
+
 
 end
